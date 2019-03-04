3a2c133bb51485cb40b7eb2de99f51b200b47330
Niko Dyer <nikodyer44@gmail.com>
2019-03-03 19:28:23 -0700

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'boards#index'
+
+resources :accounts
+
   resources :boards do
     resources :lists
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_28_203817) do
+ActiveRecord::Schema.define(version: 2019_03_04_020738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
@@ -40,6 +48,19 @@ ActiveRecord::Schema.define(version: 2019_02_28_203817) do
     t.index ["list_id"], name: "index_tasks_on_list_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "accounts", "users"
   add_foreign_key "lists", "boards"
   add_foreign_key "tasks", "lists"
 end
